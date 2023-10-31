8e78a4555d850f71d42f14c5ce3bc854aa612a97
João Paulo Duarte <joao.sousa@tangotech.com.br>
2023-10-31 11:56:31 -0300

    feat: added routes

--- App.tsx
+++ App.tsx
@@ -1,38 +1,44 @@
-
-import { StyleSheet, Text, View, ActivityIndicator,StatusBar } from 'react-native';
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-import { NativeBaseProvider, extendTheme } from 'native-base';
-import Loading from '@components/Loading';
-import SignIn from '@screens/SignIn';
-import SignUp from '@screens/SignUp';
-
-
-
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  StatusBar,
+} from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { NativeBaseProvider, extendTheme } from "native-base";
+import Loading from "@components/Loading";
+import SignIn from "@screens/SignIn";
+import SignUp from "@screens/SignUp";
+import { Routes } from "@routes/index";
 
 export const THEME = extendTheme({
   colors: {
     green: {
-      700: '#00875F',
-      500: '#00B37E',
+      700: "#00875F",
+      500: "#00B37E",
     },
     gray: {
-      700: '#121214',
-      600: '#202024',
-      500: '#29292E',
-      400: '#323238',
-      300: '#7C7C8A',
-      200: '#C4C4CC',
-      100: '#E1E1E6'
+      700: "#121214",
+      600: "#202024",
+      500: "#29292E",
+      400: "#323238",
+      300: "#7C7C8A",
+      200: "#C4C4CC",
+      100: "#E1E1E6",
     },
-    white: '#FFFFFF',
+    white: "#FFFFFF",
     red: {
-      500: '#F75A68'
-    }
+      500: "#F75A68",
+    },
   },
   fonts: {
-    heading: 'Roboto_700Bold',
-    body: 'Roboto_400Regular',
+    heading: "Roboto_700Bold",
+    body: "Roboto_400Regular",
   },
   fontSizes: {
     xs: 12,
@@ -43,31 +49,24 @@ export const THEME = extendTheme({
   },
   sizes: {
     14: 56,
-    33: 148
-  }
-})
+    33: 148,
+  },
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
-
-   
+    Roboto_700Bold,
+  });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar barStyle={"light-content"} backgroundColor="transparent" translucent/>
-      {
-        fontsLoaded? (
-          <SignUp/>
-
-        ):(<Loading/>)
-      }
-     
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
-
-
-
---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,21 @@
+import {createNativeStackNavigator, NativeStackNavigationProp} from '@react-navigation/native-stack';
+import SignIn from '@screens/SignIn';
+import SignUp from '@screens/SignUp';
+
+type authRoutesProps = {
+  SignIn: undefined;
+  SignUp: undefined;
+}
+
+export type authNavigationProps = NativeStackNavigationProp<authRoutesProps>
+
+const {Navigator, Screen} = createNativeStackNavigator<authRoutesProps>();
+
+export const AuthRoutes = () => {
+  return (
+    <Navigator screenOptions={{headerShown: false}}>
+      <Screen name="SignIn" component={SignIn} />
+      <Screen name="SignUp" component={SignUp} />
+    </Navigator>
+  );
+}
